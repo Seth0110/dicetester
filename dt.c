43c4c656bfe92d7815eb9850e39eea84ff81dfe7
Seth <seth.sevier@gmail.com>
2021-02-06 03:36:46 +0000

    Sides now specified at runtime

--- dt.c
+++ dt.c
@@ -2,13 +2,19 @@
  * Dice Tester - Counter tool to display statistics about the randomness of dice 
  */
 
+#include <gsl/gsl_cdf.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <gsl/gsl_cdf.h>
 
-#define SIDES 6
+int
+getSides(int argc, char **argv) {
+  int sides = 6;
+  if (argc >= 2 && atoi(argv[1]) > 0)
+    sides = atoi(argv[1]);
+  return sides;
+}
 
 double
 chiSquared(double *observed, double *expected, int sides) {
@@ -22,17 +28,26 @@ int
 main(int argc, char **argv)
 {
   char input[8];
-  int num, total;
-  double observed[SIDES], expected[SIDES];
-
+  int num, total, sides, memSize;
+  double *observed, *expected;
+  
   total = 0;
-  memset(observed, 0, sizeof observed);
-  memset(input, 0, sizeof input);
 
+  sides = getSides(argc, argv);
+  memSize = sizeof(double) * sides;
+  observed = malloc(memSize);
+  expected = malloc(memSize);
+  printf("HERE %d\n", sides);
+  for (int i = 0; i < sides; i++)
+    observed[i] = 0.0;
+  
+  printf("HERE %d\n", sides);
+  printf("Obs: %f\n", observed[0]);
   /* Load observed data */
   while (scanf("%s", input) != EOF) {
     num = atoi(input);
-    if (num >= 1 && num <= SIDES) {
+
+    if (num >= 1 && num <= sides) {
       printf("%d\n", num);
       observed[num-1] += 1;
       total += 1;
@@ -41,15 +56,15 @@ main(int argc, char **argv)
   }
 
   /* Random = flat distribution */
-  for (int i = 0; i < SIDES; i++)
-    expected[i] = (double) total / SIDES;
+  for (int i = 0; i < sides; i++)
+    expected[i] = (double) total / sides;
 
-  double chi2 = chiSquared(observed, expected, SIDES);
-  double pval = 1-gsl_cdf_chisq_P(chi2, SIDES-1);
+  double chi2 = chiSquared(observed, expected, sides);
+  double pval = 1-gsl_cdf_chisq_P(chi2, sides-1);
 
   fputs("\n########## RESULTS ##########\n", stderr);
   fputs("Num:\tExp:\tObs:\tPer:\t\n", stderr);
-  for (int i = 0; i < SIDES; i++)
+  for (int i = 0; i < sides; i++)
     fprintf(stderr, "%d:\t %2.0f\t %2.0f\t %3.0f%%\n", i+1, expected[i], observed[i], (observed[i] / total)*100);
   if (pval >= 0.1)
     fputs("\nDie is fair :)\n", stderr);
@@ -57,6 +72,9 @@ main(int argc, char **argv)
     fputs("\nDie is probably fair\n", stderr);
   else
     fputs("\nDie not fair :(\n", stderr);
+
+  free(observed);
+  free(expected);
   
   return 0;
 }
